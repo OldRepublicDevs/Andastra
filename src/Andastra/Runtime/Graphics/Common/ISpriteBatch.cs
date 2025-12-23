7c8f7c553105cc26a6064e5c956d4297597f3d2b
Benjamin Auquite <halomastar@gmail.com>
2025-12-23 07:58:37 -0600

    fix: update WalkmeshTests to use public BWM API and add missing using statements

--- src/Andastra/Runtime/Graphics/Common/ISpriteBatch.cs
+++ src/Andastra/Runtime/Graphics/Common/ISpriteBatch.cs
@@ -92,6 +92,11 @@ namespace Andastra.Runtime.Graphics
 
         public static Vector2 Zero => new Vector2(0, 0);
         public static Vector2 One => new Vector2(1, 1);
+
+        public static Vector2 operator +(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.X + right.X, left.Y + right.Y);
+        }
     }
 
     /// <summary>
