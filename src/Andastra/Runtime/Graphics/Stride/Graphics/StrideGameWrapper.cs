b9712bf3f24544eb08cd06dfbf1540a80067ddad
Benjamin Auquite <halomastar@gmail.com>
2025-12-23 15:08:13 -0600

    fix: use fully qualified global::Stride.Engine.Game to resolve namespace conflict

--- src/Andastra/Runtime/Graphics/Stride/Graphics/StrideGameWrapper.cs
+++ src/Andastra/Runtime/Graphics/Stride/Graphics/StrideGameWrapper.cs
@@ -8,7 +8,7 @@ namespace Andastra.Runtime.Stride.Graphics
     /// Wrapper for Stride.Engine.Game that provides UpdateFrame and DrawFrame events.
     /// Stride's Game class uses Update() and Draw() methods, but we need event-based callbacks.
     /// </summary>
-    public class StrideGameWrapper : Game
+    public class StrideGameWrapper : global::Stride.Engine.Game
     {
         /// <summary>
         /// Event raised before each update frame.
