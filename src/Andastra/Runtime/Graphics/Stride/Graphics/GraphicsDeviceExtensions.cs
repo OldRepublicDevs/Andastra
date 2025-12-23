2932be08f8bdaf9dd4e76592597d3d3a16401797
Benjamin Auquite <halomastar@gmail.com>
2025-12-23 08:45:33 -0600

    fix: replace GraphicsContext with CommandList in StrideFsrSystem and add Services extension method

--- src/Andastra/Runtime/Graphics/Stride/Graphics/GraphicsDeviceExtensions.cs
+++ src/Andastra/Runtime/Graphics/Stride/Graphics/GraphicsDeviceExtensions.cs
@@ -1,4 +1,5 @@
 using StrideGraphics = Stride.Graphics;
+using Stride.Engine;
 
 namespace Andastra.Runtime.Stride.Graphics
 {
@@ -6,8 +7,9 @@ namespace Andastra.Runtime.Stride.Graphics
     /// Extension methods for Stride GraphicsDevice to provide compatibility with older API.
     /// </summary>
     /// <remarks>
-    /// TODO: STUB - ImmediateContext was removed from GraphicsDevice in newer Stride versions.
+    /// TODO: STUB - ImmediateContext and Services were removed from GraphicsDevice in newer Stride versions.
     /// Need to implement proper CommandList retrieval from GraphicsDevice.ResourceFactory or CommandListPool.
+    /// Services should be obtained from Game.Services or ServiceRegistry, not from GraphicsDevice.
     /// For now, returns null to allow compilation. Code using this should handle null case.
     /// </remarks>
     public static class GraphicsDeviceExtensions
@@ -24,6 +26,24 @@ namespace Andastra.Runtime.Stride.Graphics
         {
             // TODO: STUB - Implement proper CommandList retrieval
             // In newer Stride, use: device.ResourceFactory.CreateCommandList() or get from CommandListPool
+            // For now, return null to allow compilation - calling code must handle null
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the service registry for the graphics device.
+        /// </summary>
+        /// <remarks>
+        /// TODO: STUB - GraphicsDevice.Services was removed in newer Stride versions.
+        /// Services should be obtained from Game.Services or passed as a parameter.
+        /// For now, return null to allow compilation - calling code must handle null.
+        /// </remarks>
+        [JetBrains.Annotations.CanBeNull]
+        public static object Services(this StrideGraphics.GraphicsDevice device)
+        {
+            // TODO: STUB - Implement proper Services retrieval
+            // In newer Stride, services are accessed through Game.Services, not GraphicsDevice.Services
+            // The return type should be ServiceRegistry or IServiceRegistry, but using object for now
             // For now, return null to allow compilation - calling code must handle null
             return null;
         }
