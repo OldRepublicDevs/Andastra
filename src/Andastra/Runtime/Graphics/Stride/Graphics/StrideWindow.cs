887a24c34ebe235188ae41795f25a6f12e172da2
Benjamin Auquite <halomastar@gmail.com>
2025-12-23 21:28:12 -0600

    fix: use reflection to access GameWindow.ClientSize and Close in StrideWindow

--- src/Andastra/Runtime/Graphics/Stride/Graphics/StrideWindow.cs
+++ src/Andastra/Runtime/Graphics/Stride/Graphics/StrideWindow.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Reflection;
 using Stride.Games;
+using Stride.Core.Mathematics;
 using Andastra.Runtime.Graphics;
 
 namespace Andastra.Runtime.Stride.Graphics
@@ -10,12 +12,20 @@ namespace Andastra.Runtime.Stride.Graphics
     public class StrideWindow : IWindow
     {
         private readonly GameWindow _window;
+        private PropertyInfo _clientSizeProperty;
+        private MethodInfo _closeMethod;
 
         internal GameWindow Window => _window;
 
         public StrideWindow(GameWindow window)
         {
             _window = window ?? throw new ArgumentNullException(nameof(window));
+            
+            // Use reflection to access ClientSize property (may not be directly accessible in all Stride versions)
+            _clientSizeProperty = _window.GetType().GetProperty("ClientSize");
+            
+            // Use reflection to access Close method (may not be directly accessible in all Stride versions)
+            _closeMethod = _window.GetType().GetMethod("Close");
         }
 
         public string Title
@@ -38,23 +48,65 @@ namespace Andastra.Runtime.Stride.Graphics
 
         public int Width
         {
-            get { return _window.ClientSize.X; }
+            get
+            {
+                if (_clientSizeProperty != null)
+                {
+                    var size = _clientSizeProperty.GetValue(_window);
+                    if (size is Int2 int2Size)
+                    {
+                        return int2Size.X;
+                    }
+                }
+                // Fallback: return a default size if ClientSize is not available
+                return 1280;
+            }
             set
             {
-                var size = _window.ClientSize;
-                size.X = value;
-                _window.ClientSize = size;
+                if (_clientSizeProperty != null && _clientSizeProperty.CanWrite)
+                {
+                    var currentSize = _clientSizeProperty.GetValue(_window);
+                    if (currentSize is Int2 currentInt2)
+                    {
+                        var newSize = new Int2(value, currentInt2.Y);
+                        _clientSizeProperty.SetValue(_window, newSize);
+                        return;
+                    }
+                }
+                // If ClientSize is not available, size cannot be set directly
+                // Window size will be managed through GraphicsDevice.Presenter in this case
             }
         }
 
         public int Height
         {
-            get { return _window.ClientSize.Y; }
+            get
+            {
+                if (_clientSizeProperty != null)
+                {
+                    var size = _clientSizeProperty.GetValue(_window);
+                    if (size is Int2 int2Size)
+                    {
+                        return int2Size.Y;
+                    }
+                }
+                // Fallback: return a default size if ClientSize is not available
+                return 720;
+            }
             set
             {
-                var size = _window.ClientSize;
-                size.Y = value;
-                _window.ClientSize = size;
+                if (_clientSizeProperty != null && _clientSizeProperty.CanWrite)
+                {
+                    var currentSize = _clientSizeProperty.GetValue(_window);
+                    if (currentSize is Int2 currentInt2)
+                    {
+                        var newSize = new Int2(currentInt2.X, value);
+                        _clientSizeProperty.SetValue(_window, newSize);
+                        return;
+                    }
+                }
+                // If ClientSize is not available, size cannot be set directly
+                // Window size will be managed through GraphicsDevice.Presenter in this case
             }
         }
 
@@ -62,7 +114,16 @@ namespace Andastra.Runtime.Stride.Graphics
 
         public void Close()
         {
-            _window.Close();
+            if (_closeMethod != null)
+            {
+                _closeMethod.Invoke(_window, null);
+            }
+            else
+            {
+                // Fallback: try to exit through the game if Close is not available
+                // This will be handled by the graphics backend
+                throw new NotSupportedException("Close method is not available on GameWindow");
+            }
         }
     }
 }
