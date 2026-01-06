ef7aff480014d8ee7e61f2ab5483170cfdd5272b
Benjamin Auquite <halomastar@gmail.com>
2026-01-06 02:03:48 -0600

    refactor: set AssemblyName for Andastra.Parsing and remove unused using statements

--- src/Andastra/Parsing/Resource/Formats/NCS/NCSDecomp/OutputRepairProcessor.cs
+++ src/Andastra/Parsing/Resource/Formats/NCS/NCSDecomp/OutputRepairProcessor.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using Andastra.Parsing.Common;
 using JetBrains.Annotations;
 
 namespace Andastra.Parsing.Formats.NCS.NCSDecomp
