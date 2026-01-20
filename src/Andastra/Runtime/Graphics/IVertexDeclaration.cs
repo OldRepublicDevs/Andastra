246ed586024f93cb2ceefd532b2dac73c8d4b881
Benjamin Auquite <halomastar@gmail.com>
2026-01-20 14:10:52 -0600

    docs(graphics): document OpenGL vertex format system functions and addresses

--- src/Andastra/Runtime/Graphics/IVertexDeclaration.cs
+++ src/Andastra/Runtime/Graphics/IVertexDeclaration.cs
@@ -5,14 +5,26 @@ namespace Andastra.Runtime.Graphics
     /// </summary>
     /// <remarks>
     /// Vertex Declaration Interface:
-    /// - [TODO: Function name] @ (K1: TODO: Find this address, TSL: TODO: Find this address address) DirectX vertex format system
-    /// - Located via string references: "Disable Vertex Buffer Objects" @ 0x007b56bc (VBO option)
-    /// - "glVertexAttrib4fvNV" @ 0x007b7d24, "glVertexAttrib3fvNV" @ 0x007b7d38, "glVertexAttrib2fvNV" @ 0x007b7d4c
-    /// - Original implementation: DirectX 8/9 flexible vertex format (FVF) or vertex declarations
+    /// - GLRender::SetInterleavedBuffer @ (K1: 0x00425520, TSL: TODO: Find address) OpenGL vertex format/declaration system
+    /// - VertexPrimitiveFlat::SetupArrays @ (K1: 0x00479020, TSL: TODO: Find address) Main vertex format setup orchestrator
+    /// - Located via string references: "Disable Vertex Buffer Objects" @ 0x0073d6c0 (VBO option)
+    /// - "glVertexAttrib4fvNV" @ 0x0073fcd0, "glVertexAttrib3fvNV" @ 0x0073fce4, "glVertexAttrib2fvNV" @ 0x0073fcf8
+    /// - Original implementation: OpenGL vertex arrays using glVertexPointer, glNormalPointer, glTexCoordPointer, glColorPointer
     /// - Vertex formats: Define vertex structure (position, normal, texture coordinates, colors, etc.)
-    /// - FVF: Flexible Vertex Format codes define vertex layout (D3DFVF_* constants)
+    /// - SetInterleavedBuffer: Sets up interleaved vertex buffer format based on bit flags:
+    ///   - Bit 0 (0x01): Position (glVertexPointer, 3 floats)
+    ///   - Bit 1 (0x02): Texture coordinate 0 (glTexCoordPointer, 2 floats)
+    ///   - Bit 2 (0x04): Texture coordinate 1/lightmap (glTexCoordPointer, 2 floats)
+    ///   - Bit 3 (0x08): Additional texture coordinates
+    ///   - Bit 5 (0x20): Normal (glNormalPointer, 3 floats)
+    ///   - Bit 6 (0x40): Color (glColorPointer, 4 unsigned bytes)
+    /// - Supporting functions:
+    ///   - GLRender::SetVertexBuffer @ (K1: 0x00425900) - Sets vertex position pointer
+    ///   - GLRender::SetNormalBuffer @ (K1: 0x004259e0) - Sets normal pointer
+    ///   - GLRender::SetTexCoordBuffer @ (K1: 0x00425a40) - Sets texture coordinate pointer
+    ///   - GLRender::SetColorBuffer @ (K1: 0x00425970) - Sets color pointer
     /// - This interface: Abstraction layer for modern graphics APIs (DirectX 11/12, OpenGL, Vulkan)
-    /// - Note: Modern APIs use vertex declarations/elements instead of FVF codes
+    /// - Note: Modern APIs use vertex declarations/elements instead of legacy OpenGL client state
     /// </remarks>
     public interface IVertexDeclaration
     {
