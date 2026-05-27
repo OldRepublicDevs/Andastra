c9c8738d447ecaacc519896465a6967550eb7d13
Your Name <halomastar@gmail.com>
2026-05-27 09:23:54 -0500

    test(kotorcli): cover find-strref --no-ncs behavior

--- tests/KotorCLI.Tests/FindStrRefCommandTests.cs
+++ tests/KotorCLI.Tests/FindStrRefCommandTests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
+using BioWare.Common;
 using BioWare.Resource;
+using BioWare.Resource.Formats.NCS;
 using BioWare.Resource.Formats.SSF;
 using KotorCLI.Commands;
 using KotorCLI.Logging;
@@ -143,6 +145,78 @@ namespace KotorCLI.Tests
             }
         }
 
+        [Test]
+        public void Execute_NcsOnly_FindsWithoutNoNcsFlag()
+        {
+            string installRoot = CreateInstallWithNcsStrRef(424242);
+            try
+            {
+                var logger = new StandardLogger();
+                int exitCode = FindStrRefCommand.Execute(
+                    424242,
+                    installRoot,
+                    overrideOnly: true,
+                    noOverride: false,
+                    noChitin: true,
+                    noModules: true,
+                    noNcs: false,
+                    logger);
+                Assert.That(exitCode, Is.EqualTo(0));
+            }
+            finally
+            {
+                DeleteDirectorySafe(installRoot);
+            }
+        }
+
+        [Test]
+        public void Execute_NcsOnly_SkipsWithNoNcsFlag()
+        {
+            string installRoot = CreateInstallWithNcsStrRef(424242);
+            try
+            {
+                var logger = new StandardLogger();
+                int exitCode = FindStrRefCommand.Execute(
+                    424242,
+                    installRoot,
+                    overrideOnly: true,
+                    noOverride: false,
+                    noChitin: true,
+                    noModules: true,
+                    noNcs: true,
+                    logger);
+                Assert.That(exitCode, Is.EqualTo(1));
+            }
+            finally
+            {
+                DeleteDirectorySafe(installRoot);
+            }
+        }
+
+        [Test]
+        public void Execute_NoNcs_StillFindsSsfHit()
+        {
+            string installRoot = CreateInstallWithStrRef(77777);
+            try
+            {
+                var logger = new StandardLogger();
+                int exitCode = FindStrRefCommand.Execute(
+                    77777,
+                    installRoot,
+                    overrideOnly: true,
+                    noOverride: false,
+                    noChitin: true,
+                    noModules: true,
+                    noNcs: true,
+                    logger);
+                Assert.That(exitCode, Is.EqualTo(0));
+            }
+            finally
+            {
+                DeleteDirectorySafe(installRoot);
+            }
+        }
+
         private static string CreateInstallWithStrRef(int strref)
         {
             string installRoot = Path.Combine(Path.GetTempPath(), "kotorcli-strref-" + Guid.NewGuid().ToString("N"));
@@ -155,6 +229,21 @@ namespace KotorCLI.Tests
             ssf.SetData(SSFSound.BATTLE_CRY_1, strref);
             byte[] bytes = SSFAuto.BytesSsf(ssf);
             File.WriteAllBytes(Path.Combine(overrideDir, "test_set.ssf"), bytes);
+
+            return installRoot;
+        }
+
+        private static string CreateInstallWithNcsStrRef(int strref)
+        {
+            string installRoot = Path.Combine(Path.GetTempPath(), "kotorcli-ncs-strref-" + Guid.NewGuid().ToString("N"));
+            string overrideDir = Path.Combine(installRoot, "Override");
+            Directory.CreateDirectory(overrideDir);
+            File.WriteAllBytes(Path.Combine(installRoot, "SWKOTOR.EXE"), new byte[0]);
+            File.WriteAllBytes(Path.Combine(installRoot, "chitin.key"), new byte[0]);
+
+            NCS ncs = NCSAuto.CompileNss("void main() { int n = " + strref + "; }", BioWareGame.K1);
+            byte[] bytes = NCSAuto.BytesNcs(ncs);
+            File.WriteAllBytes(Path.Combine(overrideDir, "test_script.ncs"), bytes);
 
             return installRoot;
         }
