6969bb2f84583b4f20a3823ad0db4c1fb3f252e5
Your Name <halomastar@gmail.com>
2026-05-27 11:17:55 -0500

    test(kotorcli): cover install happy path with fake game directory

--- tests/KotorCLI.Tests/InstallCommandTests.cs
+++ tests/KotorCLI.Tests/InstallCommandTests.cs
@@ -1,5 +1,10 @@
 using System;
 using System.IO;
+using BioWare.Common;
+using BioWare.Resource;
+using BioWare.Resource.Formats.ERF;
+using BioWare.Resource.Formats.GFF;
+using BioWare.Resource.Formats.GFF.Generics.UTC;
 using KotorCLI.Commands;
 using KotorCLI.Logging;
 using NUnit.Framework;
@@ -89,6 +94,52 @@ file = ""test.mod""
                 DeleteDirectorySafe(projectDir);
                 DeleteDirectorySafe(fakeInstallDir);
             }
+        }
+
+        [Test]
+        public void Execute_WithPackedModAndFakeInstallDir_CopiesToModules()
+        {
+            string projectDir = Path.Combine(Path.GetTempPath(), "kotorcli-install-happy-" + Guid.NewGuid().ToString("N"));
+            string fakeInstallDir = Path.Combine(Path.GetTempPath(), "kotorcli-install-game-" + Guid.NewGuid().ToString("N"));
+            string originalDirectory = Directory.GetCurrentDirectory();
+
+            try
+            {
+                Directory.CreateDirectory(projectDir);
+                Directory.CreateDirectory(fakeInstallDir);
+                File.WriteAllText(Path.Combine(fakeInstallDir, "chitin.key"), "fake-key");
+                File.WriteAllText(Path.Combine(projectDir, "kotorcli.cfg"), MinimalConfig);
+
+                string modPath = Path.Combine(projectDir, "test.mod");
+                WriteModWithUtc(modPath, "install_creature");
+
+                Directory.SetCurrentDirectory(projectDir);
+
+                var logger = new StandardLogger();
+                int exitCode = InstallCommand.Execute(new[] { "default" }, fakeInstallDir, false, false, logger);
+
+                string installedModPath = Path.Combine(fakeInstallDir, "modules", "test.mod");
+                Assert.That(exitCode, Is.EqualTo(0));
+                Assert.That(File.Exists(installedModPath), Is.True);
+
+                ERF installedMod = ERFAuto.ReadErf(installedModPath, ResourceType.MOD);
+                Assert.That(installedMod.Get("install_creature", ResourceType.UTC), Is.Not.Null);
+            }
+            finally
+            {
+                Directory.SetCurrentDirectory(originalDirectory);
+                DeleteDirectorySafe(projectDir);
+                DeleteDirectorySafe(fakeInstallDir);
+            }
+        }
+
+        private static void WriteModWithUtc(string modPath, string resref)
+        {
+            GFF gff = UTCHelpers.DismantleUtc(new UTC(), BioWareGame.K1);
+            byte[] bytes = GFFAuto.BytesGff(gff, ResourceType.UTC);
+            var mod = new ERF(ERFType.MOD);
+            mod.SetData(resref, ResourceType.UTC, bytes);
+            ERFAuto.WriteErf(mod, modPath, ResourceType.MOD);
         }
 
         private static void DeleteDirectorySafe(string path)
