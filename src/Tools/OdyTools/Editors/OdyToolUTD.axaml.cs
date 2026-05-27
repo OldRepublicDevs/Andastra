f5f696a87c86036efd796af876188b93342ccfa9
Your Name <halomastar@gmail.com>
2026-05-27 08:35:25 -0500

    feat(odytools): find references on utc/utd/utp conversation combo

--- src/Tools/OdyTools/Editors/OdyToolUTD.axaml.cs
+++ src/Tools/OdyTools/Editors/OdyToolUTD.axaml.cs
@@ -641,10 +641,16 @@ namespace OdyTools.Editors
             openInEditorItem.Click += (sender, e) => EditConversation();
             contextMenu.Items.Add(openInEditorItem);
 
+            var findReferencesItem = new MenuItem { Header = "Find References", IsEnabled = false };
+            findReferencesItem.Click += (sender, e) => ConversationReferenceHelper.FindAndShowConversationReferences(this, comboBox, _installation);
+            contextMenu.Items.Add(findReferencesItem);
+
             void UpdateOpenEnabled(object s, EventArgs e)
             {
                 string text = comboBox.SelectedItem?.ToString() ?? comboBox.Text ?? string.Empty;
-                openInEditorItem.IsEnabled = !string.IsNullOrWhiteSpace(text);
+                bool hasConversation = !string.IsNullOrWhiteSpace(text);
+                openInEditorItem.IsEnabled = hasConversation;
+                findReferencesItem.IsEnabled = hasConversation && _installation?.Installation != null;
             }
             comboBox.SelectionChanged += UpdateOpenEnabled;
             contextMenu.Opened += (s, e) => UpdateOpenEnabled(s, e);
