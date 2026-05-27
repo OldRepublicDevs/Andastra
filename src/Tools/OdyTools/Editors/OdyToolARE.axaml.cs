c41ddb4fc52d7bb317f5b6c7658b62676350733a
Your Name <halomastar@gmail.com>
2026-05-27 08:31:35 -0500

    feat(odytools): add script find-references on are editor

--- src/Tools/OdyTools/Editors/OdyToolARE.axaml.cs
+++ src/Tools/OdyTools/Editors/OdyToolARE.axaml.cs
@@ -1734,6 +1734,14 @@ namespace OdyTools.Editors
             openInEditorItem.Click += (sender, e) => OpenScriptInEditor(comboBox, scriptTypeName);
             menuItems.Add(openInEditorItem);
 
+            var findReferencesItem = new MenuItem
+            {
+                Header = "Find References",
+                IsEnabled = false
+            };
+            findReferencesItem.Click += (sender, e) => ScriptReferenceHelper.FindAndShowScriptReferences(this, comboBox, _installation);
+            menuItems.Add(findReferencesItem);
+
             // "Create New Script" menu item - creates a new NSS file
             var createNewItem = new MenuItem
             {
@@ -1756,6 +1764,7 @@ namespace OdyTools.Editors
                 string text = comboBox.SelectedItem?.ToString() ?? comboBox.Text ?? string.Empty;
                 bool hasScriptName = !string.IsNullOrWhiteSpace(text);
                 openInEditorItem.IsEnabled = hasScriptName;
+                findReferencesItem.IsEnabled = hasScriptName && _installation != null;
                 viewLocationItem.IsEnabled = hasScriptName;
             }
 
