63624fccec1a919a4c4bb92976fd4fde18705ee1
Your Name <halomastar@gmail.com>
2026-05-27 09:40:27 -0500

    test(odytools): cover strref collect with ncs scan disabled

--- tests/OdyTools.Tests/StrRefReferenceHelperTests.cs
+++ tests/OdyTools.Tests/StrRefReferenceHelperTests.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using BioWare.Common;
 using BioWare.Resource;
+using BioWare.Resource.Formats.NCS;
 using BioWare.Resource.Formats.SSF;
 using BioWare.Tools;
 using NUnit.Framework;
@@ -67,6 +69,35 @@ namespace OdyTools.Tests
             }
         }
 
+        [Test]
+        public void CollectStrRefReferences_NoNcsScan_SkipsNcsHit()
+        {
+            const int targetStrRef = 424242;
+            string installRoot = CreateInstallWithNcsStrRef(targetStrRef);
+            try
+            {
+                var installation = new OdyInstallation(installRoot, "Test");
+                var options = new ReferenceSearchOptions
+                {
+                    SearchOverride = true,
+                    SearchChitin = false,
+                    SearchModules = false,
+                    IncludeNcsStrRefScan = false
+                };
+
+                List<ReferenceSearchResult> results = StrRefReferenceHelper.CollectStrRefReferences(
+                    targetStrRef,
+                    installation,
+                    options);
+
+                Assert.That(results, Is.Empty);
+            }
+            finally
+            {
+                DeleteDirectorySafe(installRoot);
+            }
+        }
+
         private static string CreateInstallWithStrRef(int strref)
         {
             string installRoot = Path.Combine(Path.GetTempPath(), "odytools-strref-" + Guid.NewGuid().ToString("N"));
@@ -79,6 +110,21 @@ namespace OdyTools.Tests
             ssf.SetData(SSFSound.BATTLE_CRY_1, strref);
             byte[] bytes = SSFAuto.BytesSsf(ssf);
             File.WriteAllBytes(Path.Combine(overrideDir, "test_set.ssf"), bytes);
+
+            return installRoot;
+        }
+
+        private static string CreateInstallWithNcsStrRef(int strref)
+        {
+            string installRoot = Path.Combine(Path.GetTempPath(), "odytools-ncs-strref-" + Guid.NewGuid().ToString("N"));
+            string overrideDir = Path.Combine(installRoot, "Override");
+            Directory.CreateDirectory(overrideDir);
+            File.WriteAllBytes(Path.Combine(installRoot, "SWKOTOR.EXE"), new byte[0]);
+            File.WriteAllBytes(Path.Combine(installRoot, "chitin.key"), new byte[0]);
+
+            NCS ncs = NCSAuto.CompileNss("void main() { int n = " + strref + "; }", BioWareGame.K1);
+            byte[] bytes = NCSAuto.BytesNcs(ncs);
+            File.WriteAllBytes(Path.Combine(overrideDir, "test_script.ncs"), bytes);
 
             return installRoot;
         }
