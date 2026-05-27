a62a592a4c954234897b3377b1c5432d843c99b7
Your Name <halomastar@gmail.com>
2026-05-27 11:32:10 -0500

    test(kotorcli): cover list verbose with package source includes

--- tests/KotorCLI.Tests/ListCommandTests.cs
+++ tests/KotorCLI.Tests/ListCommandTests.cs
@@ -89,6 +89,46 @@ description = ""Default target""
             }
         }
 
+        [Test]
+        public void Execute_VerboseWithPackageSourceInclude_ExitsZero()
+        {
+            string projectDir = Path.Combine(Path.GetTempPath(), "kotorcli-list-verbose-" + Guid.NewGuid().ToString("N"));
+            string originalDirectory = Directory.GetCurrentDirectory();
+            const string sourceConfig = @"[package]
+name = ""testpack""
+
+  [package.sources]
+  include = ""src/**/*.json""
+
+[target]
+name = ""default""
+file = ""test.mod""
+description = ""Default target""
+";
+
+            try
+            {
+                Directory.CreateDirectory(projectDir);
+                File.WriteAllText(Path.Combine(projectDir, "kotorcli.cfg"), sourceConfig);
+
+                string srcDir = Path.Combine(projectDir, "src");
+                Directory.CreateDirectory(srcDir);
+                File.WriteAllText(Path.Combine(srcDir, "sample.utc.json"), "{}");
+
+                Directory.SetCurrentDirectory(projectDir);
+
+                var logger = new StandardLogger();
+                int exitCode = ListCommand.Execute(Array.Empty<string>(), false, true, logger);
+
+                Assert.That(exitCode, Is.EqualTo(0));
+            }
+            finally
+            {
+                Directory.SetCurrentDirectory(originalDirectory);
+                DeleteDirectorySafe(projectDir);
+            }
+        }
+
         private static void DeleteDirectorySafe(string path)
         {
             try
